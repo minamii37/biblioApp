99d43840093694160756eb1b3b799449f99bd485
minamii <49612620+minamii37@users.noreply.github.com>
2022-08-02 15:43:43 +0900

    Initial commit

---
+++ src/main/java/com/example/biblioapp/BiblioAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.biblioapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblioAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblioAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/biblioapp/BiblioAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.biblioapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiblioAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
